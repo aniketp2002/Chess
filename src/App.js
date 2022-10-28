2fac9efce1b7c4dd14910c2bd13501aaf820e8af
darshankamble04 <darshankamble7371@gmail.com>
2022-10-28 13:46:52 +0530

    routes fixed

--- src/App.js
+++ src/App.js
@@ -2,23 +2,26 @@ import { startTransition, useEffect, useState } from "react";
 import Online from "./components/Online"
 import Offline from "./components/Offline";
 import Landing from "./components/Landing";
-import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 function App() {
   const [showlanding, setshowlanding] = useState(true);
   // const invertlanding = ()=>{
   //   showlanding==true?setshowlanding(false):setshowlanding(true);
   // }
-return (
-  <>
-  {showlanding && <Landing hidelanding={setshowlanding}/>}
-  <Routes>
-                  <Route exact path='/' element={<Landing/>}></Route>
-                 
-            </Routes>
+  return (
+    <>
+      {/* {showlanding && <Landing hidelanding={setshowlanding} />} */}
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Landing />} />
+          <Route exact path='/online' element={<Online />} />
+          <Route exact path='/offline' element={<Offline />} />
+        </Routes>
+      </Router>
 
 
-  </>
-);
+    </>
+  );
 }
-   
+
 export default App;
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,35 +5,27 @@ import Offline from './Offline';
 import Online from './Online';
 
 import landingimg from '../assets/landingpage.jpg';
-import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
-const Landing = ({hidelanding}) => {
-  
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+const Landing = ({ hidelanding }) => {
+
   return (
     <>
-        <Router>
-        <div className="contnr">
+      <div className="contnr">
         {/* <img src={landingimg} className="bdy"/> */}
         <div className="head">
-            <div className="heading">ChessMaster</div>
-            <div className="subheading">SAY <span className='fcColor'>"YES!"</span>  NEW <span className='fcColor'>VICTORIES</span> <br />AND INCREASED <span className='fcColor'>THINKING!</span></div>
-            <br />
-            <div className="">
-              <Link to="/online"><button type="button" class="btn1" onClick={()=>hidelanding(false)}>Online</button></Link> 
-              <Link to="/offline"><button type="button" class="btn1  mx-3 " onClick={()=>hidelanding(false)}>Offline</button></Link>
-            </div>
-            <Routes>
-                  <Route exact path='/' element={<Landing/>}></Route>
-                 <Route exact path='/online' element={<Online/>}></Route>
-                 <Route exact path='/offline' element={<Offline/>}></Route>
-            </Routes>
+          <div className="heading">ChessMaster</div>
+          <div className="subheading">SAY <span className='fcColor'>"YES!"</span>  NEW <span className='fcColor'>VICTORIES</span> <br />AND INCREASED <span className='fcColor'>THINKING!</span></div>
+          <br />
+          <div className="">
+            <Link to="/online"><button type="button" class="btn1" onClick={() => hidelanding(false)}>Online</button></Link>
+            <Link to="/offline"><button type="button" class="btn1  mx-3 " onClick={() => hidelanding(false)}>Offline</button></Link>
+          </div>
         </div>
-        <img src={landingimg} className="bdy"/>
-        </div>
-        </Router>
-       
-    </>     
-        
-    
+        <img src={landingimg} className="bdy" />
+      </div>
+    </>
+
+
   )
 }
 
