6a91527376b0dc5cc633be6253445991a7fb1f0b
Aniket Patil <aniketspatil02@gmail.com>
2022-10-28 10:07:11 +0530

    Co-authored-by: darshankamble7371@gmail.com <darshankamble7371@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import { startTransition, useEffect, useState } from "react";
-
+import Online from "./components/Online"
 import Offline from "./components/Offline";
 import Landing from "./components/Landing";
 import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
 function App() {
+  const [showlanding, setshowlanding] = useState(true);
+  // const invertlanding = ()=>{
+  //   showlanding==true?setshowlanding(false):setshowlanding(true);
+  // }
 return (
   <>
-    <Landing/>
+  {showlanding && <Landing hidelanding={setshowlanding}/>}
+  <Routes>
+                  <Route exact path='/' element={<Landing/>}></Route>
+                 
+            </Routes>
+
+
   </>
 );
 }
---
+++ src/components/Online.js
@@ -0,0 +1,10 @@
+import React from 'react'
+function Online() {
+    return (  
+        <>
+            <div>hiii</div>
+        </>
+    );
+}
+
+export default Online;
