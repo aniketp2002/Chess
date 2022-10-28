bf72d0729ae678663f8ca4aa786b47a2e3f40f41
Aniket Patil <aniketspatil02@gmail.com>
2022-10-28 09:34:56 +0530

    components linked

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import "./landing.css"
+import Offline from './Offline';
+import Online from './Online';
+
 import landingimg from '../assets/landingpage.jpg';
-const Landing = () => {
+import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+const Landing = ({hidelanding}) => {
+  
   return (
     <>
+        <Router>
         <div className="contnr">
         {/* <img src={landingimg} className="bdy"/> */}
         <div className="head">
@@ -11,12 +18,19 @@ const Landing = () => {
             <div className="subheading">SAY <span className='fcColor'>"YES!"</span>  NEW <span className='fcColor'>VICTORIES</span> <br />AND INCREASED <span className='fcColor'>THINKING!</span></div>
             <br />
             <div className="">
-              <button type="button" class="btn1">Online</button>
-              <button type="button" class="btn1  mx-3 ">Offline</button>
+              <Link to="/online"><button type="button" class="btn1" onClick={()=>hidelanding(false)}>Online</button></Link> 
+              <Link to="/offline"><button type="button" class="btn1  mx-3 " onClick={()=>hidelanding(false)}>Offline</button></Link>
             </div>
+            <Routes>
+                  <Route exact path='/' element={<Landing/>}></Route>
+                 <Route exact path='/online' element={<Online/>}></Route>
+                 <Route exact path='/offline' element={<Offline/>}></Route>
+            </Routes>
         </div>
         <img src={landingimg} className="bdy"/>
         </div>
+        </Router>
+       
     </>     
         
     
